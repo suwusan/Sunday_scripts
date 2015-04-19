1c6dc5e50a5a38fb038db62139f95cf373e42194
SYW <susan.wu@utsouthwestern.edu>
2015-04-19 15:41:15 -0500

    Iris

--- Sunday-pm-gapminderR.R
+++ Sunday-pm-gapminderR.R
@@ -14,3 +14,30 @@ gap.in <- read.table("output/combined_gapMinder.tsv",
                      sep = "\t",
                      header = TRUE)
 
+gap.in %>%
+  filter(pop > 15000000) %>%
+  select(country, year, pop) %>%
+  group_by(country) %>%
+  summarize(min = min (min(pop)))
+
+#Calculate the mean population per continent per year for years prior to 1990
+gap.in %>%
+  filter(year < 1990) %>%
+  select(continent, year, pop) %>%
+  group_by(continent, year) %>% 
+  summarize(mean=mean(pop)) 
+
+# Add iris 
+data(iris)
+attach(iris)
+#Load tidyr
+install.packages("tidyr", dependencies = TRUE)
+library("tidyr")
+#turn iris dataset from wide into long format
+iris.long <- gather(iris, "Measurement", "Value", 1:4)
+
+gap.in %>%
+  select(country, gdpPercap, year) %>%
+  
+
+
