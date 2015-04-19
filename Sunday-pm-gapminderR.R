d94b43a50787f8e82ca0990586437f5bea9b3a9c
SYW <susan.wu@utsouthwestern.edu>
2015-04-19 14:23:03 -0500

    Library etc

--- Sunday-pm-gapminderR.R
+++ Sunday-pm-gapminderR.R
@@ -3,19 +3,14 @@
 # susan.wu@utsouthwestern.edu
 # 04192015
 
+#install dplyr package and dependencies
+install.packages("dplyr", dependencies = TRUE)
+
+# Load dplyr library into working environment
+library("dplyr")
+
 # Read in data as tab-delimited
 gap.in <- read.table("output/combined_gapMinder.tsv", 
                      sep = "\t",
                      header = TRUE)
-for (row.number in 1:10){
-  for (col.number in 1:5) {
-    print(gap.in[row.number,col.number])
-  }
-}
-gap.in[1:10,1:5]
 
-add.me <- function(x,y){
-  (x + y)*8
-}
-
-add.me(3,4)
